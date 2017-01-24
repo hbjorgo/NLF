9e1a50889ad6ec78bfb6c167127cadab4c005589
Henning Bjørgo <henning.bjorgo@bouvet.no>
2017-01-24 08:23:26 +0100

    Added unit tests.

--- HeboTech.NLF.Loggers.Test/FormatLoggerTest.cs
+++ HeboTech.NLF.Loggers.Test/FormatLoggerTest.cs
@@ -51,18 +51,140 @@ namespace HeboTech.NLF.Loggers.Test
             Assert.AreEqual(LogLevel.DEBUG, logger.Level);
         }
 
-        /*
+        
         [TestMethod]
         public void DebugWritesDefaultFormattedMessage()
         {
-            string writerOutput = string.Empty;
             var writer = new Mock<IWriter>();
-            FormatLogger logger = new FormatLogger(writer.Object);
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
 
             logger.Debug("A", "B", "C", 3);
 
-            writer.Verify(x => x.Write("timestamp - DEBUG - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - DEBUG - C - Line 3 - B - A"), Times.Once);
         }
-        */
+
+        [TestMethod]
+        public void DebugWritesDefaultFormattedMessageAndException()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Debug("A", new Exception("ExceptionMsg"), "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - DEBUG - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("ExceptionMsg"), Times.Once);
+        }
+
+        [TestMethod]
+        public void InfoWritesDefaultFormattedMessage()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Info("A", "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - INFO - C - Line 3 - B - A"), Times.Once);
+        }
+
+        [TestMethod]
+        public void InfoWritesDefaultFormattedMessageAndException()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Info("A", new Exception("ExceptionMsg"), "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - INFO - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("ExceptionMsg"), Times.Once);
+        }
+
+        [TestMethod]
+        public void WarnWritesDefaultFormattedMessage()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Warn("A", "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - WARN - C - Line 3 - B - A"), Times.Once);
+        }
+
+        [TestMethod]
+        public void WarnWritesDefaultFormattedMessageAndException()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Warn("A", new Exception("ExceptionMsg"), "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - WARN - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("ExceptionMsg"), Times.Once);
+        }
+
+        [TestMethod]
+        public void ErrorWritesDefaultFormattedMessage()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Error("A", "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - ERROR - C - Line 3 - B - A"), Times.Once);
+        }
+
+        [TestMethod]
+        public void ErrorWritesDefaultFormattedMessageAndException()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Error("A", new Exception("ExceptionMsg"), "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - ERROR - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("ExceptionMsg"), Times.Once);
+        }
+
+        [TestMethod]
+        public void FatalWritesDefaultFormattedMessage()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Fatal("A", "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - FATAL - C - Line 3 - B - A"), Times.Once);
+        }
+
+        [TestMethod]
+        public void FatalWritesDefaultFormattedMessageAndException()
+        {
+            var writer = new Mock<IWriter>();
+            var timeProvider = new Mock<ITimeProvider>();
+            timeProvider.Setup(x => x.GetTime()).Returns(new DateTime(2016, 12, 20, 12, 0, 0));
+            FormatLogger logger = new FormatLogger(writer.Object, timeProvider.Object);
+
+            logger.Fatal("A", new Exception("ExceptionMsg"), "B", "C", 3);
+
+            writer.Verify(x => x.Write("20.12.2016 12.00.00 - FATAL - C - Line 3 - B - A"), Times.Once);
+            writer.Verify(x => x.Write("ExceptionMsg"), Times.Once);
+        }
     }
 }
